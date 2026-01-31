8f57dcfc1f47d48c01169632caafa8c12c814947
sumeyahassen <sumeyahassensumi@gmail.com>
2026-01-31 13:29:15 +0300

    modifay phone number

--- backend/test-twilio.js
+++ backend/test-twilio.js
@@ -5,7 +5,7 @@ const client = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 client.messages.create({
   body: 'Test SMS from Ethiopia Agri Platform',
   from: process.env.TWILIO_NUMBER,
-  to: '+251912380076'  // ← your own Ethiopian number for testing
+  to: '+15551234567'  // ← your own Ethiopian number for testing
 })
 .then(msg => console.log('Success! SID:', msg.sid))
 .catch(err => console.log('Error:', err.message));
