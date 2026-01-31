44113e760b0a8af4f216ffdc5ec569100bb10918
sumeyahassen <sumeyahassensumi@gmail.com>
2026-01-31 13:53:36 +0300

    Fix SSL/TLS for Render PostgreSQL

--- backend/models/index.js
+++ backend/models/index.js
@@ -3,9 +3,16 @@ require('dotenv').config();
 const { Sequelize, DataTypes } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
+  dialect: "postgres",
   logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,       // This tells Sequelize to use SSL
+      rejectUnauthorized: false // Allows Render certs
+    }
+  }
 });
+
 
 // Define User model
 const User = sequelize.define('User', {
